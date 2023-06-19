1efaa39bf8f56f74d51ceed8b821fb163db81888
Phoebe Hames <77605055+PAZHames@users.noreply.github.com>
2023-06-19 11:07:06 +0100

    add dogShip, make it move within parameters of board and not display previous versions of board

--- src/index.js
+++ src/index.js
@@ -22,22 +22,56 @@ let ship = {
 }
 
 let shipImg;
+let shipVelocityX = tileSize; //speed ship moves - alwasy moves along x plain
+
+
+// humans
+
+let humanArray = [];
+let humanWidth = tileSize * 2;
+let humanHeight = tileSize;
+let humanX = tileSize;
+let humanY = tileSize;
+
 
 window.onload = function() {
     board = document.getElementById("board");
     board.width = boardWidth;
     board.height = boardHeight;
     context = board.getContext("2d"); // used for drawing on board 
-}
+
 
 // draw initial ship
 
-// no fillStyle - needed?
-// context.fillRect(ship.x, ship.y, ship.width, ship.height);
+//load images 
+    shipImg = new Image();
+    shipImg.src = "src/images/dogShip.png";
+    shipImg.onload = function() {
+    // console.log("Image loaded successfully.");
+        context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+    }
 
-//load images
-shipImg = new Image();
-shipImg.src = "./images/ship-dog.jpeg";
-shipImg.onload = function() {
+
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", moveShip);
+}
+
+function update() {
+    requestAnimationFrame(update);
+
+    context.clearRect(0,0,board.width,board.height); //clears previous board so don't have multiple ships
+    // repeatedly drawing ship
     context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
+
+
+}
+
+function moveShip(e) {
+// e = event
+    if (e.code == "ArrowLeft" && ship.x - shipVelocityX >=0) { //ensures ship will not go off the page 
+        ship.x -= shipVelocityX; //move left - shipVeloctyX = tileSize of 1
+    }
+    else if (e.code == "ArrowRight" && ship.x + shipVelocityX + shipWidth <= board.width) { // not v clear why need both shipVel and shipWidth??
+        ship.x += shipVelocityX; //move right
+    }
 }
