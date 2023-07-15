9f0bab2443d485a55088988a76a266c7961479b6
Phoebe Hames <77605055+PAZHames@users.noreply.github.com>
2023-07-15 10:00:06 +0100

    deleted index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-let ballVelocityY = -10; // ball moving speed - minus bc moving up 
-let ballImg;
-
-// load images
-
-ballImg = new Image();
-ballImg.src= "src/images/tennisBall.png";
-
-context.drawImage(ballImg, ball.x, ball.y, ball.width, ball.height); 
